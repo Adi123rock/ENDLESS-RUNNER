1c5a2f6d06e24591429b642e62a3c7794ddaeaff
Aditya Kumar Prasad <prasadadityakumar40@gmail.com>
2025-07-08 20:35:25 +0530

    Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float playerspeed = 2;
+    public float horizontalSpeed = 3;
+    public float limit = 5.5f;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * playerspeed, Space.World);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -limit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (transform.position.x < limit)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed);
+            }
+        }
+    }
+}
